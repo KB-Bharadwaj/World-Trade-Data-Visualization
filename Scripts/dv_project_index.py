0f4c095872e28d8421cc541cbd4688a28001feed
deekshithaGarrepelly <117413701+deekshithaGarrepelly@users.noreply.github.com>
2023-10-15 15:21:13 -0400

    merging back seaborn line removed in merge commit

--- Scripts/dv_project_index.py
+++ Scripts/dv_project_index.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,url_for,request
 import matplotlib
 import matplotlib.pyplot as plt
-
+import seaborn as sb
 import numpy as np
 import pandas as pd
 #import json
